e6d58f6f11c056fb1a8f32d62c1cb855f400ed3c
Karin Prater <gahntpo@yahoo.de>
2023-09-18 09:05:52 +0300

    Initial Commit

---
+++ LocalizationProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizationProject
+//
+//  Created by Karin Prater on 18.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizationProject/LocalizationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationProjectApp.swift
+//  LocalizationProject
+//
+//  Created by Karin Prater on 18.09.23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
